4f03eceeee66a3065ed0609dd79cf2fec7b762c1
AffanK245 <Affankarikazi121@gmail.com>
2025-11-16 17:43:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,12 @@ terraform {
 resource "aws_instance" "MyEC2" {
     ami           = var.ami_id
     instance_type = var.instance_type
+    key_name      = "my-keypair" 
     tags = {
       Name = "EC2-Instance-Affan"
     }
 }
+
 
 
 
